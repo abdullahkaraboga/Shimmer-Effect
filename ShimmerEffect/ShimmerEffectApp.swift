6c4312f850715a644569cbea33dac98462d65efc
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-03-11 17:05:46 +0300

    Initial Commit

---
+++ ShimmerEffect/ShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerEffectApp.swift
+//  ShimmerEffect
+//
+//  Created by Abdullah Karaboğa on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
